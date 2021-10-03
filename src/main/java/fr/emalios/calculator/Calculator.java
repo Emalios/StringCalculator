97262988152f0bfd303a38ec0f8807427abb4eec
wasaba <patafix258@gmail.com>
2021-10-03 12:39:17 +0200

    cleaned the code + renamed some variables for a proper reading

--- src/main/java/fr/emalios/calculator/Calculator.java
+++ src/main/java/fr/emalios/calculator/Calculator.java
@@ -10,22 +10,28 @@ public class Calculator {
             return "0";
         }
         int result = 0;
-        String error=validate(number);
-        String sep = ",";
+        String error = validate(number);
+        String separator = ",";
+
         if (number.startsWith("//")){
-            sep = (number.substring(number.indexOf("//")+2,number.indexOf("\n")));
+            separator = (number.substring(number.indexOf("//")+2,number.indexOf("\n")));
             List<String> regexArray = Arrays.asList("|", "?", "+", "\\", ".", "^", "[", "]", "$", "&", ".");
-            if (regexArray.contains(sep)){
-                sep=("\\"+sep);
+
+            if (regexArray.contains(separator)){
+                separator=("\\"+separator);
             }
+
             number = number.substring(number.indexOf("\n")+1);
         }
+
         if (error.equals("")){
             String[] newlineSplit = number.split("\n");
+
             for (String part : newlineSplit) {
-                String[] split = part.split(sep);
+                String[] split = part.split(separator);
                 result += Arrays.stream(split).mapToInt(Integer::parseInt).sum();
             }
+
             return String.valueOf(result);
         } else {
             return error;
@@ -33,42 +39,49 @@ public class Calculator {
     }
 
     private String validate(String number){
-        StringBuilder error= new StringBuilder();
+        StringBuilder error = new StringBuilder();
+
         if (number.contains(",\n")){
-            if (error.length()>0) error.append("\n");
+            if (error.length() > 0) error.append("\n");
             error.append("Number expected but '\\n' found at position ").append(number.indexOf(",\n"));
         }
+
         if (number.endsWith(",")){
-            if (error.length()>0) error.append("\n");
+            if (error.length() > 0) error.append("\n");
             error.append("Number expected but EOF found.");
         }
+
         if (number.contains("-")){
-            if (error.length()>0) error.append("\n");
+            if (error.length() > 0) error.append("\n");
             error.append(detectNegative(number));
         }
+
         if (number.contains(",,")){
-            if (error.length()>0) error.append("\n");
+            if (error.length() > 0) error.append("\n");
             error.append("Number expected but ',' found at position ").append(number.indexOf(",,")+1).append("");
         }
+
         return error.toString();
     }
 
     private String detectNegative(String number){
         String numberVal = number;
-        StringBuilder result= new StringBuilder();
-        int index=0;
+        StringBuilder result = new StringBuilder();
+        int index = 0;
         while (index != -1){
             index = numberVal.indexOf("-");
             if (index != -1){
-                String val = String.valueOf(numberVal.charAt(index));
-                String val1 = String.valueOf(numberVal.charAt(index+1));
+
+                String value1 = String.valueOf(numberVal.charAt(index));
+                String value2 = String.valueOf(numberVal.charAt(index+1));
+
                 if (result.length() > 0){
-                    result.append(", ").append(val).append(val1);
+                    result.append(", ").append(value1).append(value2);
                 } else {
-                    result.append(val).append(val1);
+                    result.append(value1).append(value2);
                 }
                 index++;
-                numberVal=numberVal.substring(index);
+                numberVal = numberVal.substring(index);
             }
         }
         if (result.length() != 0) {
